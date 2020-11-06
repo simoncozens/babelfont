e1f4db1f8a00a9f32c5f154216f14f381f6384c9
Simon Cozens <simon@simon-cozens.org>
2020-11-06 12:42:37 +0000

    Bump version: 0.2.2 → 0.3.0

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/simoncozens/babelfont',
-    version='0.2.2',
+    version='0.3.0',
     zip_safe=False,
     entry_points={
         "console_scripts": [
