44918913571891a01efffbf33277d0732f2f4332
Simon Cozens <simon@simon-cozens.org>
2020-11-10 11:09:37 +0000

    Add insertpoint to make copy work

--- lib/babelfont/contour.py
+++ lib/babelfont/contour.py
@@ -1,12 +1,14 @@
 from fontParts.base.contour import BaseContour
 from fontParts.fontshell.segment import RSegment # Fine until fontshell splits...
 from babelfont import addUnderscoreProperty
+from babelfont.point import Point
 
 
 @addUnderscoreProperty("clockwise")
 class Contour(BaseContour):
     segmentClass = RSegment
-
+    def _init(self, **kwargs):
+        self._points = []
     def _lenPoints(self):
         return len(self._points)
 
@@ -28,6 +30,16 @@ class Contour(BaseContour):
             self.clockwise = False
         else:
             self.clockwise = True
+
+    def _insertPoint(self, index, position, type=None, smooth=None,
+                     name=None, identifier=None, **kwargs):
+        point = Point()
+        point.x = position[0]
+        point.y = position[1]
+        point.type = type
+        point.smooth = smooth
+        point.name = name
+        self._points.insert(index, point)
 
     @property
     def identifier(self):
