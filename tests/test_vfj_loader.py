90a4476b9ef8d92547ee1e7a7436ed82befec706
Simon Cozens <simon@simon-cozens.org>
2021-02-02 14:36:09 +0000

    Fix VFJ test

--- tests/test_vfj_loader.py
+++ tests/test_vfj_loader.py
@@ -1,18 +1,9 @@
-from munch import munchify
-import json
-from babelfont.layer import Layer
-from babelfont.convertors.fontlab import _load_glyph
 from babelfont import Babelfont
 
 def test_load_glyph():
     filename = "tests/data/Castoro Roman.vfj"
-    vfj = munchify(json.load(open(filename, "r"))).font
-    master = vfj.masters[0].fontMaster
-    l = Layer()
-    a = _load_glyph(vfj.glyphs[5], l, master)
-    assert len(a.contours[0]) == 20
-    assert len(a.contours[1]) == 4
-
-def test_loader():
-    filename = "tests/data/Castoro Roman.vfj"
     font = Babelfont.open(filename)
+
+    assert len(font["A"].contours[0]) == 20
+    assert len(font["A"].contours[1]) == 4
+
