6fceb907f5499373bf6f4f119355291ebbf98f4e
Simon Cozens <simon@simon-cozens.org>
2021-02-04 17:37:00 +0000

    Axis IDs

--- Lib/nfsf/Axis.py
+++ Lib/nfsf/Axis.py
@@ -1,15 +1,21 @@
 from .BaseObject import BaseObject
 from dataclasses import dataclass
+import uuid
 
 
 @dataclass
 class Axis(BaseObject):
     name: str
     tag: str
+    id: str = None
     min: int = None
     max: int = None
     default: int = None
 
+    def __post_init__(self):
+        if not self.id:
+            self.id = str(uuid.uuid1())
+        self._formatspecific = {}
 
     _serialize_slots = __annotations__.keys()
     _write_one_line = True
