b6252cb853502f46b848d4c2dbb8a0d4d3f8ae5c
Simon Cozens <simon@simon-cozens.org>
2021-02-05 21:15:18 +0000

    Reformat with black

--- Lib/nfsf/Glyph.py
+++ Lib/nfsf/Glyph.py
@@ -2,17 +2,17 @@ from dataclasses import dataclass
 from .BaseObject import BaseObject
 from .Layer import Layer
 
+
 @dataclass
 class Glyph(BaseObject):
-  name: str
-  category: str = "base"
-  codepoint: int = None
-  layers: [Layer] = None
+    name: str
+    category: str = "base"
+    codepoint: int = None
+    layers: [Layer] = None
 
-  _serialize_slots = ["name", "category", "codepoint"]
-  _write_one_line = True
+    _serialize_slots = ["name", "category", "codepoint"]
+    _write_one_line = True
 
-  def __post_init__(self):
-      self.layers = []
-      self._formatspecific = {}
-
+    def __post_init__(self):
+        self.layers = []
+        self._formatspecific = {}
--- Lib/nfsf/Layer.py
+++ Lib/nfsf/Layer.py
@@ -3,24 +3,25 @@ from .BaseObject import BaseObject, Color
 from .Guide import Guide
 from .Anchor import Anchor
 
+
 @dataclass
 class Layer(BaseObject):
 
-  id: str
-  width: int
-  name: str = None
-  _master: str = None
-  guides: [Guide] = None
-  shapes: list = None
-  anchors: [Anchor] = None
-  color: Color = None
-  layerIndex: int = 0
-  # hints: [Hint]
-  _background: str = None
-  isBackground: bool = False
-  location: [float] = None
-  lsb: int = None
-  rsb: int = None
+    id: str
+    width: int
+    name: str = None
+    _master: str = None
+    guides: [Guide] = None
+    shapes: list = None
+    anchors: [Anchor] = None
+    color: Color = None
+    layerIndex: int = 0
+    # hints: [Hint]
+    _background: str = None
+    isBackground: bool = False
+    location: [float] = None
+    lsb: int = None
+    rsb: int = None
 
-  _serialize_slots = __annotations__.keys()
-  _separate_items = { "shapes": True }
+    _serialize_slots = __annotations__.keys()
+    _separate_items = {"shapes": True}
