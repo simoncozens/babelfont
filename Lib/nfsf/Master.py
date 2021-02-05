aef82bd276d58061c34adddbec4d7bcf62803d9f
Simon Cozens <simon@simon-cozens.org>
2021-02-05 23:24:25 +0000

    Docs, find a master’s glyph layer

--- Lib/nfsf/Master.py
+++ Lib/nfsf/Master.py
@@ -1,17 +1,40 @@
 from dataclasses import dataclass
 from .BaseObject import BaseObject
+from .Guide import Guide
+
 
 @dataclass
 class Master(BaseObject):
-  name: str
-  id: str
-  location: dict = None
-  guides: list = None
-  xHeight: int = None
-  capHeight: int = None
-  ascender: int = None
-  descender: int = None
-  kerning: dict = None
+    """A font master.
 
-  _serialize_slots = __annotations__.keys()
-  _separate_items = { "kerning": True }
+    Attributes:
+        name (str): The user-facing master name.
+        id (str): An internal identifier for the master.
+        location (dict): A dictionary locating this master by mapping axis
+            name to designspace location.
+        guides ([Guide]): A list of master-level guidelines
+        xHeight (int): The x height of this master, in font units.
+        capHeight (int): The cap height of this master, in font units.
+        ascender (int): The ascender of this master, in font units.
+        descender (int): The descender of this master, in font units.
+        font (Font): The font that this master belongs to.
+    """
+
+    name: str
+    id: str
+    location: dict = None
+    guides: [Guide] = None
+    xHeight: int = None
+    capHeight: int = None
+    ascender: int = None
+    descender: int = None
+    kerning: dict = None
+
+    _serialize_slots = __annotations__.keys()
+    _separate_items = {"kerning": True}
+
+    def get_glyph_layer(self, glyphname):
+        g = self.font.glyphs[glyphname]
+        for layer in g.layers:
+            if layer._master == self.id:
+                return layer
