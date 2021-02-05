825751a4bfc7c85ac126a39c90248aa2a31b37f3
Simon Cozens <simon@simon-cozens.org>
2021-02-05 23:24:42 +0000

    Draw method!

--- Lib/nfsf/Layer.py
+++ Lib/nfsf/Layer.py
@@ -1,4 +1,7 @@
 from dataclasses import dataclass
+
+from fontTools.ufoLib.pointPen import PointToSegmentPen
+
 from .BaseObject import BaseObject, Color
 from .Guide import Guide
 from .Anchor import Anchor
@@ -25,3 +28,25 @@ class Layer(BaseObject):
 
     _serialize_slots = __annotations__.keys()
     _separate_items = {"shapes": True}
+
+    @property
+    def paths(self):
+        return [x for x in self.shapes if x.is_path]
+
+    @property
+    def components(self):
+        return [x for x in self.shapes if x.is_component]
+
+    def draw(self, pen):
+        pen = PointToSegmentPen(pen)
+        for path in self.paths:
+            pen.beginPath()
+            for node in path.nodes:
+                pen.addPoint(
+                    pt=(node.x, node.y),
+                    segmentType=node.pen_type,
+                    smooth=node.is_smooth,
+                )
+            pen.endPath()
+        for component in self.components:
+            pen.addComponent(component.ref, component.transformation)
