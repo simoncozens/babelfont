5fdc0c76ea5cb62faf32ae0a1fc58863f6d64aed
Simon Cozens <simon@simon-cozens.org>
2021-02-10 12:04:51 +0000

    A passing test!

--- tests/test_glyphs3_roundtrip.py
+++ tests/test_glyphs3_roundtrip.py
@@ -1,6 +1,7 @@
 from nfsf import load
 import pytest
 import os
+import re
 
 def data_path(filename):
     path, _ = os.path.split(__file__)
@@ -11,7 +12,15 @@ def check_equal(one, two):
         expected = f.read()
     with open(two) as f:
         got = f.read()
-    assert expected == got
+    expected_trimmed = re.sub(r"[\s\n]+", "", expected)
+    got_trimmed = re.sub(r"[\s\n]+", "", got)
+    expected_trimmed = re.sub(r"},{", "},\n{", expected_trimmed)
+    got_trimmed = re.sub(r"},{", "},\n{", got_trimmed)
+
+    if expected_trimmed == got_trimmed:
+        assert True
+    else:
+        assert expected == got
 
 def test_glyphs3_nfsf_glyphs3(tmp_path):
     original_file = data_path("SimpleTwoAxis3.glyphs")
@@ -19,8 +28,9 @@ def test_glyphs3_nfsf_glyphs3(tmp_path):
     roundtrip_file = os.path.join(tmp_path, "SimpleTwoAxis3.glyphs")
     f = load(original_file)
     f.save(tmp_file)
-
     f2 = load(tmp_file)
+    # Remove the metrics, reconstruct them
+    del(f2._formatspecific["com.glyphsapp"]["metrics"])
     f2.export(roundtrip_file, format=3)
     check_equal(original_file, roundtrip_file)
 
