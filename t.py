e415a4f2d69a4e35539bbdddffa2ef21764c44fd
Simon Cozens <simon@simon-cozens.org>
2021-02-11 10:45:21 +0000

    Convert cu2qu all at once to ensure interpolability

--- t.py
+++ t.py
@@ -2,28 +2,41 @@ from nfsf.convertors import Convert
 from dataclasses import asdict
 from fontTools.fontBuilder import FontBuilder
 from fontTools.pens.ttGlyphPen import TTGlyphPen
-from fontTools.pens.cu2quPen import Cu2QuPen
+from fontTools.cu2qu.ufo import glyphs_to_quadratic
 from fontTools.misc.timeTools import epoch_diff, timestampSinceEpoch
 from fontTools.ttLib.ttFont import _TTGlyphGlyf, _TTGlyphSet
 from fontTools.ttLib.tables.TupleVariation import TupleVariation
 import warnings
 
 # f = Convert("Truculenta[opsz,wdth,wght].glyphs").load()
-f = Convert("Nunito3.glyphs").load()
+# f = Convert("tests/data/GlyphsFileFormatv3.glyphs").load()
 #f = Convert("/Users/simon/others-repos/Amstelvar/sources/Roman/AmstelvarDB.designspace").load()
 #f = Convert("/Users/simon/others-repos/glyphsLib/master_ufo/GlyphsUnitTestSans.designspace").load()
-# f.save("output/test.nfsf")
+f = Convert("Nunito3.glyphs").load()
+f.save("output/test.nfsf")
 
 fb = FontBuilder(f.upm, isTTF=True)
 fb.setupGlyphOrder(list(f.glyphs.keys()))
 fb.setupCharacterMap(f.unicode_map)
 
 metrics = {}
-
+all_outlines = {}
 for g in f.glyphs.keys():
     layer = f.default_master.get_glyph_layer(g)
     metrics[g] = (layer.width, layer.lsb)
+    all_outlines[g] = []
+    for m in f.masters:
+        all_outlines[g].append(m.get_glyph_layer(g))
+
+# Now convert to quadratic, all outlines of each glyph at once
+for g in f.glyphs.keys():
+    glyphs_to_quadratic(all_outlines[g])
+
 fb.setupHorizontalMetrics(metrics)
+
+for m in f.masters:
+    glyf = {}
+    m.ttglyphset = _TTGlyphSet(fb.font, glyf, _TTGlyphGlyf)
 
 def do_a_glyph(g, master, ttglyphset):
     if g in ttglyphset._glyphs:
@@ -31,16 +44,17 @@ def do_a_glyph(g, master, ttglyphset):
     layer = master.get_glyph_layer(g)
     for c in layer.components:
         do_a_glyph(c.ref, master, ttglyphset)
-
-    pen = Cu2QuPen(TTGlyphPen(ttglyphset), 50)
+    pen = TTGlyphPen(ttglyphset)
     layer.draw(pen)
-    ttglyphset._glyphs[g] = pen.pen.glyph()
+    ttglyphset._glyphs[g] = pen.glyph()
+    all_outlines[g].append(pen.glyph())
 
 for m in f.masters:
     glyf = {}
     m.ttglyphset = _TTGlyphSet(fb.font, glyf, _TTGlyphGlyf)
     for g in f.glyphs.keys():
         do_a_glyph(g, m, m.ttglyphset)
+
 
 versionMajor =  1
 versionMinor =  0
@@ -52,8 +66,8 @@ fb.updateHead(
 )
 fb.setupGlyf(f.default_master.ttglyphset._glyphs)
 fb.setupHorizontalHeader(
-    ascent=int(f.default_master.ascender or 0),
-    descent=int(f.default_master.descender or 0),
+    ascent=int(f.default_master.ascender),
+    descent=int(f.default_master.descender),
 )
 
 fb.setupNameTable(f.names.as_nametable_dict())
