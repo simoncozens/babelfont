05bc67bf38df23a7dda62b57a2d54921c736ca41
Simon Cozens <simon@simon-cozens.org>
2024-03-14 07:40:37 +0000

    Improve typing, fix write bug

--- src/babelfont/Glyph.py
+++ src/babelfont/Glyph.py
@@ -7,36 +7,6 @@ from fontTools.misc.filenames import userNameToFileName
 import os
 
 
-class GlyphList(dict):
-    def append(self, thing):
-        self[thing.name] = thing
-
-    def write(self, stream, indent):
-        stream.write(b"[")
-        for ix, item in enumerate(self):
-            stream.write(b"\n")
-            stream.write(b"  " * (indent + 2))
-            stream.write(item, indent + 1)
-            if ix < len(self) - 1:
-                stream.write(b", ")
-            else:
-                stream.write(b"\n")
-        stream.write(b"]")
-
-    def __iter__(self):
-        self._n = 0
-        self._values = list(self.values())
-        return self
-
-    def __next__(self):
-        if self._n < len(self._values):
-            result = self._values[self._n]
-            self._n += 1
-            return result
-        else:
-            raise StopIteration
-
-
 @dataclass
 class _GlyphFields:
     name: str
@@ -57,3 +27,34 @@ class Glyph(BaseObject, _GlyphFields):
     @property
     def babelfont_filename(self):
         return os.path.join("glyphs", (userNameToFileName(self.name) + ".nfsglyph"))
+
+
+class GlyphList(dict):
+    def append(self, thing):
+        self[thing.name] = thing
+
+    def write(self, stream, indent):
+        stream.write(b"[")
+        for ix, item in enumerate(self):
+            stream.write(b"\n")
+            stream.write(b"  " * (indent + 2))
+            item.write(stream, indent + 1)
+            if ix < len(self) - 1:
+                stream.write(b", ")
+            else:
+                stream.write(b"\n")
+        stream.write(b"]")
+
+    def __iter__(self) -> "GlyphList":
+        self._n = 0
+        self._values = list(self.values())
+        return self
+
+    def __next__(self) -> Glyph:
+        if self._n < len(self._values):
+            result = self._values[self._n]
+            self._n += 1
+            return result
+        else:
+            raise StopIteration
+
