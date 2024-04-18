363d4877ea08f88b48b389c71acf5c4e63284e1c
Simon Cozens <simon@simon-cozens.org>
2024-04-18 11:56:49 +0100

    Filters should take args

--- src/babelfont/fontFilters/background.py
+++ src/babelfont/fontFilters/background.py
@@ -1,12 +1,14 @@
 from babelfont.Font import Font
 
-def zero_background_width(font: Font):
+
+def zero_background_width(font: Font, args=None):
     for glyph in font.glyphs:
         for layer in glyph.layers:
             if layer.isBackground:
                 layer.width = 0
 
-def decompose_backgrounds(font: Font):
+
+def decompose_backgrounds(font: Font, args=None):
     for glyph in font.glyphs:
         for layer in glyph.layers:
             if layer.isBackground:
--- src/babelfont/fontFilters/marks.py
+++ src/babelfont/fontFilters/marks.py
@@ -1,10 +1,13 @@
 from babelfont.Font import Font
 from glyphsLib import glyphdata
 
-def zero_mark_widths(font: Font):
+
+def zero_mark_widths(font: Font, args=None):
     for glyph in font.glyphs:
         glyphinfo = glyphdata.get_glyph(glyph.name)
-        if glyphinfo and not (glyphinfo.category == "Mark" and glyphinfo.subCategory == "Nonspacing"):
+        if glyphinfo and not (
+            glyphinfo.category == "Mark" and glyphinfo.subCategory == "Nonspacing"
+        ):
             continue
         for layer in glyph.layers:
             layer.width = 0
