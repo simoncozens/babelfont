3c97ed823d95cfbff470725d3df6846f83c7ae91
Simon Cozens <simon@simon-cozens.org>
2024-04-18 11:57:12 +0100

    cu2qu filter

--- src/babelfont/fontFilters/__init__.py
+++ src/babelfont/fontFilters/__init__.py
@@ -3,6 +3,7 @@ from .marks import zero_mark_widths
 from .rename import rename_glyphs
 from .dropUnexported import drop_unexported_glyphs
 from .decomposeMixed import decompose_mixed_glyphs
+from .cu2qu import cubic_to_quadratic
 
 FILTERS = {
     "zeroBackgroundWidths": zero_background_width,
@@ -11,4 +12,5 @@ FILTERS = {
     "renameGlyphs": rename_glyphs,
     "decomposeMixedGlyphs": decompose_mixed_glyphs,
     "dropUnexportedGlyphs": drop_unexported_glyphs,
+    "cubicToQuadratic": cubic_to_quadratic,
 }
---
+++ src/babelfont/fontFilters/cu2qu.py
@@ -0,0 +1,22 @@
+import logging
+
+from fontTools.cu2qu.ufo import glyphs_to_quadratic
+
+from babelfont.Font import Font
+
+
+logger = logging.getLogger(__name__)
+
+
+def cubic_to_quadratic(font: Font, args: dict = {}):
+    reverse_direction = args.get("reverseDirection", True)
+    for glyph in font.glyphs:
+        master_layers = [l for l in glyph.layers if l._master]
+        if not master_layers:
+            continue
+        try:
+            glyphs_to_quadratic(master_layers, reverse_direction=reverse_direction)
+        except Exception:
+            logger.warning(
+                "Problem converting glyph %s to quadratic. (Probably incompatible) " % g
+            )
