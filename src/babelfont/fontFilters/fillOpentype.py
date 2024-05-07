d1b6fdfe2ef8c4dfaea872589f3a442864027131
Simon Cozens <simon@simon-cozens.org>
2024-05-07 18:57:51 +0100

    Tidy up fill opentype

--- src/babelfont/fontFilters/fillOpentype.py
+++ src/babelfont/fontFilters/fillOpentype.py
@@ -10,34 +10,7 @@ logger = logging.getLogger(__name__)
 def _default(font, table, key, value, round=otRound):
     if (table, key) not in font.custom_opentype_values:
         font.custom_opentype_values[(table, key)] = round(value)
-
-
-def fallback_ascender(font):
-    return font.upm * 0.8
-
-
-def fallback_descender(font):
-    return font.upm * -0.2
-
-
-def fallback_cap_height(font):
-    return font.upm * 0.7
-
-
-def fallback_x_height(font):
-    return font.upm * 0.5
-
-
-def fallback_linegap(font):
-    return font.upm * 0.2
-
-
-def fallback_underline_position(font):
-    return font.upm * -0.075
-
-
-def fallback_underline_thickness(font):
-    return font.upm * 0.05
+    return font.custom_opentype_values[(table, key)]
 
 
 def fill_opentype_values(font: Font, args=None):
@@ -49,6 +22,8 @@ def fill_opentype_values(font: Font, args=None):
         for metric in metrics:
             if callable(metric):
                 return metric(font)
+            if isinstance(metric, (int, float)):
+                return metric
             if (
                 metric in font.default_master.metrics
                 and font.default_master.metrics[metric] is not None
@@ -62,57 +37,61 @@ def fill_opentype_values(font: Font, args=None):
     _default(font, "head", "fontRevision", version_decimal)
     _default(font, "head", "created", timestampSinceEpoch(font.date.timestamp()))
     _default(font, "head", "lowestRecPPEM", 10)
-    _default(
+    ascender = _default(
         font,
         "hhea",
         "ascent",
-        _fallback_metric("hheaAscender", "ascender", fallback_ascender),
+        _fallback_metric("hheaAscender", "ascender", font.upm * 0.8),
     )
-    _default(
+    descender = _default(
         font,
         "hhea",
         "descent",
-        _fallback_metric("hheaDescender", "descender", fallback_descender),
+        _fallback_metric("hheaDescender", "descender", font.upm * -0.2),
     )
     _default(font, "hhea", "lineGap", _fallback_metric("hheaLineGap"))
-    _default(font, "OS/2", "usWinAscent", _fallback_metric("winAscent", "ascender"))
-    _default(font, "OS/2", "usWinDescent", _fallback_metric("winDescent", "descender"))
+    _default(font, "OS/2", "usWinAscent", _fallback_metric("winAscent", ascender))
+    _default(font, "OS/2", "usWinDescent", _fallback_metric("winDescent", descender))
     # WinDescent should be positive
     if ("OS/2", "usWinDescent") in font.custom_opentype_values:
         font.custom_opentype_values[("OS/2", "usWinDescent")] = abs(
             font.custom_opentype_values[("OS/2", "usWinDescent")]
         )
+    _default(font, "OS/2", "sTypoAscender", _fallback_metric("typoAscender", ascender))
     _default(
-        font,
-        "OS/2",
-        "sTypoAscender",
-        _fallback_metric("typoAscender", "ascender", fallback_ascender),
-    )
-    _default(
-        font,
-        "OS/2",
-        "sTypoDescender",
-        _fallback_metric("typoDescender", "descender", fallback_descender),
+        font, "OS/2", "sTypoDescender", _fallback_metric("typoDescender", descender)
     )
     _default(
         font,
         "OS/2",
         "sTypoLineGap",
-        _fallback_metric("typoLineGap", "lineGap", fallback_linegap),
+        _fallback_metric("typoLineGap", "lineGap", font.upm * 0.2),
     )
-    _default(font, "OS/2", "sxHeight", _fallback_metric("xHeight", fallback_x_height))
+    x_height = _default(
+        font, "OS/2", "sxHeight", _fallback_metric("xHeight", font.upm * 0.5)
+    )
+    _default(font, "OS/2", "sCapHeight", _fallback_metric("capHeight", font.upm * 0.7))
     _default(
-        font, "OS/2", "sCapHeight", _fallback_metric("capHeight", fallback_cap_height)
+        font,
+        "OS/2",
+        "yStrikeoutPosition",
+        _fallback_metric("strikeoutPosition", x_height * 0.6),
     )
     _default(
         font,
         "post",
         "underlinePosition",
-        _fallback_metric("postUnderlinePosition", fallback_underline_position),
+        _fallback_metric("underlinePosition", font.upm * -0.075),
     )
     _default(
         font,
         "post",
         "underlineThickness",
-        _fallback_metric("postUnderlineThickness", fallback_underline_thickness),
+        _fallback_metric("underlineThickness", font.upm * 0.05),
+    )
+    _default(
+        font,
+        "OS/2",
+        "yStrikeoutSize",
+        _fallback_metric("underlineThickness", font.upm * 0.05),
     )
