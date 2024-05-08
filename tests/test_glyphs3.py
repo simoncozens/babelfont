4acf254e47b241b9cd70c6a8cb13779d40f4f19c
Simon Cozens <simon@simon-cozens.org>
2024-05-08 10:03:29 +0100

    Add a test for loading designspaces and maps from Glyphs 3 files

--- tests/test_glyphs3.py
+++ tests/test_glyphs3.py
@@ -4,3 +4,19 @@ from babelfont import load
 def test_load_open_shape():
     font = load("tests/data/GlyphsFileFormatv3.glyphs")
     assert font.glyphs["A"].layers[1].shapes[0].closed == False
+
+
+def test_designspace():
+    font = load("tests/data/Designspace.glyphs")
+    assert len(font.axes) == 1
+    assert font.axes[0].name.get_default() == "Weight"
+    assert font.axes[0].tag == "wght"
+    # Axes values are in userspace units
+    assert font.axes[0].minimum == 100
+    assert font.axes[0].maximum == 600
+    # Master locations are in designspace units
+    assert font.masters[0].location == {"wght": 1}
+    assert font.masters[1].location == {"wght": 199}
+    # Instance locations are in designspace units
+    assert font.instances[0].location == {"wght": 1}
+    assert font.instances[1].location == {"wght": 7}
