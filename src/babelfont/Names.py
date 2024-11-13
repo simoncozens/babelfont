eca530367ce6ce8e0cef2a03dd9832cd4ac1d532
Simon Cozens <simon@simon-cozens.org>
2024-11-13 13:23:32 +0000

    Try to sort out the naming madness

--- src/babelfont/Names.py
+++ src/babelfont/Names.py
@@ -1,5 +1,5 @@
 from .BaseObject import BaseObject, I18NDictionary
-from dataclasses import dataclass, asdict
+from dataclasses import dataclass, asdict, fields
 
 OPENTYPE_NAMES = [
     "copyright",
@@ -32,35 +32,35 @@ OPENTYPE_NAMES = [
 class Names(BaseObject):
     """A table of global, localizable names for the font."""
 
-    familyName: I18NDictionary = None
-    designer: I18NDictionary = None
-    designerURL: I18NDictionary = None
-    manufacturer: I18NDictionary = None
-    manufacturerURL: I18NDictionary = None
-    license: I18NDictionary = None
-    licenseURL: I18NDictionary = None
-    version: I18NDictionary = None
-    uniqueID: I18NDictionary = None
-    postscriptName: I18NDictionary = None
-    fullName: I18NDictionary = None
-    description: I18NDictionary = None
-    typographicFamily: I18NDictionary = None
-    typographicSubfamily: I18NDictionary = None
-    compatibleFullName: I18NDictionary = None
-    sampleText: I18NDictionary = None
-    WWSFamilyName: I18NDictionary = None
-    WWSSubfamilyName: I18NDictionary = None
-    copyright: I18NDictionary = None
-    styleMapFamilyName: I18NDictionary = None
-    styleMapStyleName: I18NDictionary = None
-    styleName: I18NDictionary = None
-    trademark: I18NDictionary = None
-    preferredSubfamilyName: I18NDictionary = None
+    familyName: I18NDictionary = None  # Name ID 1 if styleMapFamilyName is not present
+    styleName: I18NDictionary = None  # Name ID 17 if no typographicSubfamily
+
+    copyright: I18NDictionary = None  # Name ID 0
+    styleMapFamilyName: I18NDictionary = None  # Name ID 1
+    styleMapStyleName: I18NDictionary = None  # Name ID 2
+    uniqueID: I18NDictionary = None  # Name ID 3
+    fullName: I18NDictionary = None  # Name ID 4
+    version: I18NDictionary = None  # Name ID 5
+    postscriptName: I18NDictionary = None  # Name ID 6
+    trademark: I18NDictionary = None  # Name ID 7
+    manufacturer: I18NDictionary = None  # Name ID 8
+    designer: I18NDictionary = None  # Name ID 9
+    description: I18NDictionary = None  # Name ID 10
+    manufacturerURL: I18NDictionary = None  # Name ID 11
+    designerURL: I18NDictionary = None  # Name ID 12
+    license: I18NDictionary = None  # Name ID 13
+    licenseURL: I18NDictionary = None  # Name ID 14
+    typographicFamily: I18NDictionary = None  # Name ID 16
+    typographicSubfamily: I18NDictionary = None  # Name ID 17
+    compatibleFullName: I18NDictionary = None  # Name ID 18
+    sampleText: I18NDictionary = None  # Name ID 19
+    WWSFamilyName: I18NDictionary = None  # Name ID 21
+    WWSSubfamilyName: I18NDictionary = None  # Name ID 22
 
     def __post_init__(self):
-        for k in self.__dataclass_fields__.keys():
-            if not getattr(self, k):
-                setattr(self, k, I18NDictionary())
+        for k in fields(self):
+            if not getattr(self, k.name):
+                setattr(self, k.name, I18NDictionary())
 
     def as_nametable_dict(self):
         rv = {}
